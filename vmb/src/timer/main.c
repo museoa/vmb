8a47a01fcb0c059640f694b9438a14c526724c42
ruckert <ruckert@a22778cc-470f-459f-8348-c59b68ef04d0>
2008-04-15 07:41:11 +0000

    the timer device in its first unix version
    git-svn-id: https://svn.code.sf.net/p/vmb/src/trunk@84 a22778cc-470f-459f-8348-c59b68ef04d0

--- vmb/src/timer/main.c
+++ vmb/src/timer/main.c
@@ -28,21 +28,58 @@
    reference to the functions needed and provided by the
    vmb library.
 */
-
-
+#include <unistd.h>
+#include <pthread.h>
+#include <string.h>
 #include "vmb.h"
 #include "option.h"
 #include "param.h"
+#include "bus-arith.h"
 
 #define RAMSIZE 8
 
 static unsigned char ram[RAMSIZE];
+static pthread_mutex_t ms_mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t ms_cond = PTHREAD_COND_INITIALIZER;
+static long int ms;
+
+static void clean_up_ms_mutex(void *_dummy)
+{ pthread_mutex_unlock(&ms_mutex); /* needed if canceled waiting */
+}
 
 static void ram_clean(void)
 /* clean the ram, done after power on and reset */
 { int i;
   for (i=0;i<RAMSIZE;i++)
     ram[i] = 0;
+  { int rc = pthread_mutex_lock(&ms_mutex);
+    if (rc) 
+    { vmb_errormsg("Locking ms mutex failed");
+      pthread_exit(NULL);
+    }
+  }
+  ms = 0;
+  { int rc = pthread_mutex_unlock(&ms_mutex);
+    if (rc) 
+    { vmb_errormsg("Unlocking ms mutex failed");
+      pthread_exit(NULL);
+    }
+  }
+}
+
+static void wait_for_non_zero_ram(void)
+{ 
+  { int rc = pthread_mutex_lock(&ms_mutex);
+    if (rc) 
+    { vmb_errormsg("Locking ms mutex failed");
+      pthread_exit(NULL);
+    }
+  }
+  pthread_cleanup_push(clean_up_ms_mutex,NULL);
+  /* in the meantime the event might have happend */
+  while (ms == 0)
+     pthread_cond_wait(&ms_cond,&ms_mutex);
+  pthread_cleanup_pop(1);
 }
 
 
@@ -56,7 +93,6 @@ void vmb_reset(void)
 { ram_clean();
 }
 
-static long int ms;
 
 void vmb_put_payload(unsigned int offset,int size, unsigned char *payload)
 /* this function is called if some other device on the virtual bus
@@ -68,21 +104,53 @@ void vmb_put_payload(unsigned int offset,int size, unsigned char *payload)
 { 
    memmove(ram+offset,payload,size);
    /* protect ms by mutex and set up condition variable */
-   ms = chartoint(ram)<<4;
-   ms = ms + chartoint(ram+4);
+  { int rc = pthread_mutex_lock(&ms_mutex);
+    if (rc) 
+    { vmb_errormsg("Locking ms mutex failed");
+      pthread_exit(NULL);
+    }
+  }
+  ms = chartoint(ram)<<4;
+  ms = ms + chartoint(ram+4);
+  if (ms != 0)
+  {  int rc = pthread_cond_signal(&ms_cond);
+     if (rc) 
+     { vmb_errormsg("Locking ms mutex failed");
+       pthread_exit(NULL);
+     }
+  }
+  { int rc = pthread_mutex_unlock(&ms_mutex);
+    if (rc) 
+    { vmb_errormsg("Unlocking ms mutex failed");
+      pthread_exit(NULL);
+    }
+  }
 }
 
 #include <sys/select.h>
 void sleep_ms(long int ms)
 { /* sleep the specified number of miliseconds */
-  select();
+  struct timeval timeout;
+  while (ms > 0 && vmb_connected)
+  { if (ms > 5000)
+    { timeout.tv_sec = 5;
+      timeout.tv_usec = 0;
+      ms = ms - 5000;
+    }
+    else
+    { timeout.tv_sec =  ms / 1000;
+      timeout.tv_usec = (ms % 1000)*1000;
+      ms = 0;
+    }
+    select(0,NULL,NULL,NULL,&timeout);
+  }
 }
 
 
 int main(int argc, char *argv[])
 {
  param_init(argc, argv);
- size = 8;
+ vmb_size = 8;
  vmb_debugs("%s ",vmb_program_name);
  vmb_debugs("%s ", version);
  vmb_debugs("host: %s ",host);
@@ -98,12 +166,10 @@ int main(int argc, char *argv[])
  
  while (vmb_connected)
    { if (ms == 0) 
-     { /* if timer value is zero, use condition variable to
-            wait until timer value non zero */
-     }
+       wait_for_non_zero_ram();
      else 
      { sleep_ms(ms);
-     if (ms != 0) /* ther might have been a write in between */
+       if (ms != 0 && vmb_connected) /* there might have been a write in between */
          vmb_raise_interrupt(interrupt);  
      }
    }
