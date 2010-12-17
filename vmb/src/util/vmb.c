c9a7c443133bf5b92c93d86a1616f0cfa758dabf
ruckert <ruckert@a22778cc-470f-459f-8348-c59b68ef04d0>
2010-12-17 08:51:32 +0000

    moving the timed wait function to a separate file. Then only programs using it must be linked against the librt library.
    git-svn-id: https://svn.code.sf.net/p/vmb/src/trunk@223 a22778cc-470f-459f-8348-c59b68ef04d0

--- vmb/src/util/vmb.c
+++ vmb/src/util/vmb.c
@@ -29,7 +29,7 @@ void vmb_raise_reset(device_info *vmb)
 
 
 #ifndef WIN32
-static void clean_up_event_mutex(void *vmb)
+void clean_up_event_mutex(void *vmb)
 { pthread_mutex_unlock(&(((device_info *)vmb)->event_mutex)); /* needed if canceled waiting */
 }
 #endif
@@ -77,46 +77,6 @@ void vmb_raise_interrupt(device_info *vmb, unsigned char i)
 }
 
 
-
-void vmb_wait_for_event_timed(device_info *vmb, int ms)
-/* waits for a  power off, reset, disconnect, or an interrupt
-   or untim the Time in ms expires.
-*/
-{ 
-#ifndef WIN32
-  int w = 0;
-#define WAIT_TIMEOUT ETIMEDOUT
-  struct timespec ts;
-  clock_gettime(CLOCK_REALTIME, &ts);
-  ts.tv_sec += ms/1000;
-  ts.tv_nsec += (ms%1000)*1000;
-  { int rc = pthread_mutex_lock(&vmb->event_mutex);
-    if (rc) 
-    { vmb_error(__LINE__,"Locking event mutex failed");
-      pthread_exit(NULL);
-    }
-  }
-  pthread_cleanup_push(clean_up_event_mutex,vmb);
-#else
-  DWORD w = 0;
-#endif
-  /* in the meantime the event might have happend */
-  while (vmb->power &&
-         !vmb->reset_flag && 
-         vmb->connected &&
-	     vmb->interrupt_lo == 0 &&
-		 vmb->interrupt_hi ==0 &&
-         !vmb->cancel_wait_for_event &&
-		 w != WAIT_TIMEOUT
-         )
-#ifdef WIN32
-     w = WaitForSingleObject(vmb->hevent,ms);
-#else
-     pthread_cond_timedwait(&vmb->event_cond,&vmb->event_mutex, &ts);
-  pthread_cleanup_pop(1);
-#endif
-  vmb->cancel_wait_for_event=0;
-}
 
 
 void vmb_wait_for_event(device_info *vmb)
