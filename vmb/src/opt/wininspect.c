eb9bc275663fd05c46472892036e738a1fe88cac
ruckert <ruckert@a22778cc-470f-459f-8348-c59b68ef04d0>
2014-10-16 15:25:17 +0000

    moving vmb independent files to winopt
    git-svn-id: https://svn.code.sf.net/p/vmb/src/trunk@462 a22778cc-470f-459f-8348-c59b68ef04d0

--- vmb/src/opt/wininspect.c
+++
@@ -1,5 +0,0 @@
-#include <windows.h>
-#include "option.h"
-#include "inspect.h"
-
-struct inspector_def inspector[1] = {0};
--- vmb/src/opt/winlog.h
+++
@@ -1,4 +0,0 @@
-#include <windows.h>
-
-extern HWND hLog; /* may be used to position the data editor */
-extern HWND CreateLog(HWND hParent,HINSTANCE hInst);
