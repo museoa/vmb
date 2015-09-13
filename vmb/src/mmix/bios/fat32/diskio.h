135430c08f114d7b8c85cf7f99dd8c00c4288e62
ruckert <ruckert@a22778cc-470f-459f-8348-c59b68ef04d0>
2015-09-13 08:29:24 +0000

    removing mmix (to mmix repository) moving src/mmix/bios/ to src/bios
    git-svn-id: https://svn.code.sf.net/p/vmb/src/trunk@521 a22778cc-470f-459f-8348-c59b68ef04d0

--- vmb/src/mmix/bios/fat32/diskio.h
+++
@@ -1,6 +0,0 @@
-
-
-
-extern bool disk_init(void);
-extern bool disk_read(unsigned int sector, unsigned int count, BYTE * buffer);
-extern bool disk_write(unsigned int sector, unsigned int count, BYTE * buffer);
--- vmb/src/mmix/mmixal-win.ch
+++
@@ -1,8 +0,0 @@
-
-@x
-#include <time.h>
-@y
-#include <time.h>
-
-#pragma warning(disable : 4146 4244 4996) 
-@z
